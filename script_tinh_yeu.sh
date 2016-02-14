aba8d71259265e43ca2a0be0a8984c4b09523f33
Tientham <tientham@gmail.com>
2016-02-14 16:46:29 +0100

    Adding script tinh yeu

---
+++ script_tinh_yeu.sh
@@ -0,0 +1,18 @@
+# !bin/sh
+echo "TienTham script tinh yeu - version 1"
+sleep 1
+echo "Date: `date`"
+sleep 1
+echo "Tham ah..."
+sleep 1
+echo "Anh muon noi rang........"
+sleep 1
+echo "...."
+sleep 1
+echo "......."
+sleep 1
+echo "Anh yeu em!"
+sleep 1
+echo "I love you!"
+sleep 1
+echo "Ti amo!"
